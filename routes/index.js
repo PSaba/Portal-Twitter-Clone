9a63efa519fa456100863fc2018b017c5be241fe
Priyanka Saba <112psaba112@gmail.com>
2018-02-07 11:24:04 -0800

    login pages

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', 
-      name: "PriyankaSaba", username: "PSaba", tweets: 8, followers: 22 });
+      name: "PriyankaSaba", username: "PSaba", tweets: 8, followers: 22 ,
+      posts: [{name: "hi", username: "wellthen", date: "July 30", post: "what????", image: "/images/backimg.png"}]});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/loginpage', function(req, res, next){
+  res.render('loginpage');
+});
+
+router.get('/signuppage', function(req, res, next){
+  res.render('signuppage');
+});
 module.exports = router;
